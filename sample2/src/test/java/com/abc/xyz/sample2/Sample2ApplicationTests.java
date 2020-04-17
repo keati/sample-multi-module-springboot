9341d3fe5d7ee3f8c658a77ddcb94f62ad2bfd67
Kameswara R Eati <keati@users.noreply.github.com>
2020-04-17 12:52:41 -0400

    added sample files
    added sample files

---
+++ sample2/src/test/java/com/abc/xyz/sample2/Sample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.xyz.sample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
